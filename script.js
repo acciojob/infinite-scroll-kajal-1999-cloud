bf2fc69105b5bcf86cae1e29d04144cb41180916
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-13 11:08:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 //your code here!
 
+//your code here!
+// Function to add more list items
+function addMoreItems() {
+    const list = document.getElementById('infi-list');
+    const lastListItem = list.lastElementChild;
+
+    for (let i = 1; i <= 2; i++) {
+        const newItem = document.createElement('li');
+        newItem.textContent = `List Item ${list.childElementCount + i}`;
+        list.appendChild(newItem);
+    }
+}
+
+// Event listener to detect when the user scrolls to the end of the list
+document.addEventListener('scroll', function () {
+    const list = document.getElementById('infi-list');
+    const lastListItem = list.lastElementChild;
+    const lastItemOffset = lastListItem.offsetTop + lastListItem.clientHeight;
+    const pageOffset = window.pageYOffset + window.innerHeight;
+
+    if (pageOffset > lastItemOffset - 20) {
+        // Load more items when the user is near the end of the list
+        addMoreItems();
+    }
+});
+
+// Initially add about 10 list items
+addMoreItems();
